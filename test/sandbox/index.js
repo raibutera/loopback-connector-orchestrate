744d294a12ba3f54003ee3e03e3452090634d03b
Raimondo Butera <rai@rbutera.com>
2015-04-29 14:18:42 +0100

    add tools module

--- test/sandbox/index.js
+++ test/sandbox/index.js
@@ -1,6 +1,7 @@
 var orchestrate = require('orchestrate');
 var debug = require('debug')('loopback:connector:orchestrate:SANDBOX');
 var $Promise = require('bluebird');
+var characters = require('../fakedata/characters.json');
 
 orchestrate.ApiEndPoint = "api.aws-eu-west-1.orchestrate.io";
 var db = orchestrate('efbe86d4-e9fd-4801-afb6-3135446859b5');
@@ -10,3 +11,11 @@ db.ping().then(function () {
 }).fail(function (error) {
     debug('failed to connect: ', error);
 });
+
+
+db.post('testing', characters[1]).then(function (result) {
+    debug('result', result.toJSON());
+}).fail(function (err) {
+    debug('error', err);
+});
+
