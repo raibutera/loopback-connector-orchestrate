08e91fb43dbc85cbc7d1eefb4846244648307451
Raimondo Butera <rai@rbutera.com>
2015-04-29 15:09:08 +0100

    add basic create implementation

--- test/sandbox/index.js
+++ test/sandbox/index.js
@@ -2,6 +2,7 @@ var orchestrate = require('orchestrate');
 var debug = require('debug')('loopback:connector:orchestrate:SANDBOX');
 var $Promise = require('bluebird');
 var characters = require('../fakedata/characters.json');
+var orcTools = require('../../lib/orctools');
 
 orchestrate.ApiEndPoint = "api.aws-eu-west-1.orchestrate.io";
 var db = orchestrate('efbe86d4-e9fd-4801-afb6-3135446859b5');
@@ -13,9 +14,15 @@ db.ping().then(function () {
 });
 
 
-db.post('testing', characters[1]).then(function (result) {
-    debug('result', result.toJSON());
-}).fail(function (err) {
-    debug('error', err);
-});
+//db.post('testing', characters[1]).then(function (result) {
+//    debug('created new object @ ', orcTools.extractKey(result));
+//}).fail(function (err) {
+//    debug('error', err);
+// });
 
+
+db.get('testing', 'lol').then(function (success) {
+    debug('success:', success);
+}).fail(function (err) {
+    debug('error:', err.toJSON());
+});
